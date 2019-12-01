74c8c6c678acec263908686a9ae55de84e888e89
Matt Cosand <matt@cosand.net>
2019-11-30 22:01:32 -0800

    day 1 part 2 (part 1 in Excel)

---
+++ day-01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace day_01
+{
+  class Program
+  {
+    static long GetFuel(long mass)
+    {
+      if (mass <= 0) return 0;
+      long part = Math.Max(0, (long)Math.Floor(mass / 3.0) - 2);
+      return part + GetFuel(part);
+    }
+
+    static void Main(string[] args)
+    {
+      //var input = new[] { "100756" };
+      var input = File.ReadAllLines("input.txt");
+      var sum = input.Select(f => long.Parse(f)).Sum(f => GetFuel(f));
+      Console.WriteLine(sum);
+    }
+  }
+}
