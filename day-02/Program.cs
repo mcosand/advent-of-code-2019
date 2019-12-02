0f217de9d08cdac54520f717f8afb30e290acbcf
Matt Cosand <matt@cosand.net>
2019-12-01 21:24:47 -0800

    day 2 part 2

--- day-02/Program.cs
+++ day-02/Program.cs
@@ -12,32 +12,38 @@ namespace day_02
       input = "2,3,0,3,99";
       input = "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,9,19,1,19,5,23,2,23,13,27,1,10,27,31,2,31,6,35,1,5,35,39,1,39,10,43,2,9,43,47,1,47,5,51,2,51,9,55,1,13,55,59,1,13,59,63,1,6,63,67,2,13,67,71,1,10,71,75,2,13,75,79,1,5,79,83,2,83,9,87,2,87,13,91,1,91,5,95,2,9,95,99,1,99,5,103,1,2,103,107,1,10,107,0,99,2,14,0,0";
 
-      var bytes = input.Split(',').Select(f => int.Parse(f)).ToArray();
-
-      bytes[1] = 12;
-      bytes[2] = 2;
-
-      int pc = 0;
-      while (bytes[pc] != 99)
+      for (var nounverb=0; nounverb < 10000; nounverb++)
       {
-        switch (bytes[pc])
+        var bytes = input.Split(',').Select(f => int.Parse(f)).ToArray();
+
+        bytes[1] = nounverb / 100;
+        bytes[2] = nounverb % 100;
+
+        int pc = 0;
+        while (bytes[pc] != 99)
         {
-          case 1:
-            bytes[bytes[pc + 3]] = bytes[bytes[pc + 1]] + bytes[bytes[pc + 2]];
-            break;
+          switch (bytes[pc])
+          {
+            case 1:
+              bytes[bytes[pc + 3]] = bytes[bytes[pc + 1]] + bytes[bytes[pc + 2]];
+              break;
 
-          case 2:
-            bytes[bytes[pc + 3]] = bytes[bytes[pc + 1]] * bytes[bytes[pc + 2]];
-            break;
+            case 2:
+              bytes[bytes[pc + 3]] = bytes[bytes[pc + 1]] * bytes[bytes[pc + 2]];
+              break;
 
-          default:
-            break;
+            default:
+              break;
+          }
+
+          pc += 4;
         }
 
-        pc += 4;
+        if (bytes[0] == 19690720)
+        {
+          Console.WriteLine($"Noun: {nounverb / 100} Verb: {nounverb % 100}");
+        }
       }
-
-      var output = bytes[0];
     }
   }
 }
