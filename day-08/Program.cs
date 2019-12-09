3e23611d6049469d820f1e1c0b2c53de47531f5f
Matt Cosand <matt@cosand.net>
2019-12-08 20:35:53 -0800

    day 8 part 1

---
+++ day-08/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace day_08
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int width = 25; int height = 6; string input = File.ReadAllText("input.txt");
+
+      //width = 3; height = 2; input = "123456789012";
+      
+      var outp = Enumerable.Range(0, input.Length / (width * height))
+        .Select(f => input.Substring(f * width * height, width * height))
+        .OrderBy(f => f.Count(c => c == '0'))
+        .Select(f => f.Count(c => c == '1') * f.Count(c => c == '2'))
+        .First();
+    }
+  }
+}
